7e809ca4f2e7fbf6b7544336f7d5a33c7cd560e2
Star Brilliant <m13253@hotmail.com>
2013-10-23 23:00:59 +0800

    Add GPL 3.0 license

--- analysis.py
+++ analysis.py
@@ -9,10 +9,10 @@ NAMES = ['沈祖凡', '陈雯婷', '麻瑀谌', '冯炅', '马晓涵', '陶宛�
 out = open('output.csv', 'w', encoding='utf-8')
 out.write(','.join(['Time']+NAMES))
 out.write('\n')
-filenames = [i for i in os.listdir() if re.match('res-([0-9]+).html', i)]
+filenames = [i for i in os.listdir() if re.match('crawl-([0-9]+).html', i)]
 filenames.sort()
 for filename in filenames:
-    out.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(re.findall('res-([0-9]+).html', filename)[0]))))
+    out.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(re.findall('crawl-([0-9]+).html', filename)[0]))))
     out.write(',')
     with open(filename, 'r', encoding='gbk') as f:
         s = f.read()
