07478b36e340fef5ee0ca8ab6ca2f285a794a4af
Star Brilliant <m13253@hotmail.com>
2017-07-04 01:24:59 +0800

    autopep8

--- analysis.py
+++ analysis.py
@@ -7,10 +7,9 @@ import os
 NAMES = ['\u6c88\u7956\u51e1', '\u9648\u96ef\u5a77', '\u9ebb\u7440\u8c0c', '\u51af\u7085', '\u9a6c\u6653\u6db5', '\u9676\u5b9b\u742a']
 
 out = open('output.csv', 'w', encoding='utf-8-sig', newline='\r\n')
-out.write(','.join(['Time']+NAMES))
+out.write(','.join(['Time'] + NAMES))
 out.write('\n')
-filenames = [i for i in os.listdir() if re.match('crawl-([0-9]+).html$', i)]
-filenames.sort()
+filenames = sorted([i for i in os.listdir() if re.match('crawl-([0-9]+).html$', i)])
 for filename in filenames:
     try:
         out.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(re.findall('crawl-([0-9]+).html', filename)[0]))))
